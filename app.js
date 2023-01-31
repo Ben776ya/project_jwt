352de4683c04a066175fe043f70259dd1b4994f6
Ben776ya <40816784+Ben776ya@users.noreply.github.com>
2023-01-31 02:59:58 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -4,16 +4,20 @@ const dotenv=require('dotenv');
 const { UserRouter } = require('./routes/users');
 const { memosRouter } = require('./routes/memos');
 const { User } = require('./models/User');
+const cors = require('cors');
 const jwt = require("jsonwebtoken");
 dotenv.config(); // require('dotenv').config()
 //mongodb
 mongoose.connect
-("mongodb+srv://Noobenyy101:azertyuiop123@cluster0.mctfqj9.mongodb.net/users?retryWrites=true&w=majority")
+(process.env.DB_LOG)
 .then(()=>console.log("connected to mongodb atlas"))
 .catch(err=>console.log(err))
 
 //express
 const app=express();
+app.use(cors({
+    origin: '*'//allow all requests 
+}));
 
 app.use(express.static("./public"))
 
@@ -29,47 +33,47 @@ app.use(express.json())
 //   }))
 
 //injection de token
-app.use((req,res,next)=>{
-
-    const token = req.header("x-auth-token");
-
-    if (!token) return res.status(401).json({ message: "Access denied, No token provided" });
-
-    try { 
-
-        const decoded = jwt.verify(token, "secretkeyappearhere"); 
-
-        req._userId = decoded; 
-
-        next(); 
-
-    } catch (ex) { 
-
-        res.status(400).send("Invalid Token."); 
-
-    }  
-})
+// 
 
   
 
 
-app.use('/users',UserRouter)
+app.use('/register',UserRouter)
+
 
 app.get('/hi',(req,res)=>{
     res.send({message:"hi"});
 })
+app.use('/',(req,res,next)=>{
 
-// check authentification (gard / interceptor)
-/*app.use((req,res,next)=>{
+        const token = req.header("x-auth-token");
+    
+        if (!token) return res.status(401).json({ message: "Access denied, No token provided" });
+    
+        try { 
+    
+            const decoded = jwt.verify(token, process.env.TOKEN_SECRET); 
+    
+            req._userId = decoded; 
+    
+            next(); 
+    
+        } catch (ex) { 
+    
+            res.status(400).send("Invalid Token."); 
+    
+        }  
+    });
 
-    if(!req.user.login)
-        return res.status(403).json({message:"you need to login first"})
-    next();
-})*/
 
 app.use('/memos',memosRouter)
+    
 
-const port = 3000
+
+
+
+
+const port = process.env.PORT || 3000
 app.listen(port, ()=>{
     console.log('server listening on port : ',port)
 })
